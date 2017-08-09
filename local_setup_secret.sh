2a98311ac12e9088133dbac185e60ba0b247c00d
bensonz <mr.bz@hotmail.com>
2017-08-09 16:57:05 +0800

    changed ecr key name

--- local_setup_secret.sh
+++ local_setup_secret.sh
@@ -8,7 +8,7 @@ fi
 
 AWS_ACCOUNT="493490470276"
 AWS_REGION="cn-north-1"
-if [ "x$SECRET_NAME" == "x" ]; then SECRET_NAME=${AWS_REGION}-ecr-registry-key; fi
+if [ "x$SECRET_NAME" == "x" ]; then SECRET_NAME=aws-ecr-${AWS_REGION}; fi
 TOKEN=`aws ecr --region=$AWS_REGION get-authorization-token --output text --query authorizationData[].authorizationToken | base64 -D | cut -f2 -d:`
 DOCKER_CFG_SECRET=`printf '{"%s":{"username":"AWS","password":"%s"}}' "https://${AWS_ACCOUNT}.dkr.ecr.${AWS_REGION}.amazonaws.com.cn" "${TOKEN}" | base64 | tr -d '\n'`
 
